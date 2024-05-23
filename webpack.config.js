b6036e9bb263992d9d47808c2b468371209e060f
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2024-05-23 17:32:08 +0530

    🧑‍💻 update webpack.config.js for dotenv file load

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    // Other webpack configuration options...
-    module: {
-      rules: [
-        // Other rules...
-        {
-         test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'images/', // Output directory for the images
-              },
+  // Other webpack configuration options...
+  plugins: [
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+      // Other rules...
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/', // Output directory for the images
             },
-          ],
-        },
-      ],
-    },
-    };
-    
+          },
+        ],
+      },
+    ],
+  },
+<<<<<<< HEAD
+=======
+  plugins: [
+    new Dotenv(),
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+>>>>>>> origin/master
+};
